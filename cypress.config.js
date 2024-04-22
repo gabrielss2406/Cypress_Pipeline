c35d50830cab7976c9ca1a01d6f597fc10536806
Gabriel Siqueira <59343269+gabrielss2406@users.noreply.github.com>
2024-04-22 20:39:04 -0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "3u4h3h",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
